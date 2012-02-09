785a1c9ba62d229080975328d5060b35ea5b707e
Quentin Dexheimer <qdexheimer@gmail.com>
2012-02-09 14:53:13 +0100

    Indentation

--- puppet/initnfs.pp
+++ puppet/initnfs.pp
@@ -1,17 +1,17 @@
 class nfs::install {
-package { "nfs-kernel-server":
-ensure => installed,
-}
+	package { "nfs-kernel-server":
+		ensure => installed,
+	}
 }
 class nfs::service {
-service { "nfs":
-ensure => running,
-hasstatus => true,
-hasrestart => true,
-enable => true,
-require => Class["nfs::install"],
-}
+	service { "nfs":
+		ensure => running,
+		hasstatus => true,
+		hasrestart => true,
+		enable => true,
+		require => Class["nfs::install"],
+	}
 }
 class nfs {
-include nfs::install,nfs::service
+	include nfs::install,nfs::service
 }
