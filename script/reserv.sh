509bfb24e3e0fee643ce6846b2c360963701e005
Riwan Blonde <blond0392@etudiant.univ-nancy2.fr>
2012-02-09 14:41:22 +0100

    Ajout du script de réservation Modification du carnet de route Finalisation (work great!!) du script d'installation de puppet

---
+++ script/reserv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#Formulaire
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+echo -n "Nom de la réservation : "
+read nom
+echo -n "Nombre de machine à réserver : "
+read nbr_nodes
+echo -n "Temps de la réservation (h:mm:ss) : "
+read tmps
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#Réservation des machines
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+echo "Réservation de "$nbr_nodes" machine(s) pour "$tmps" heure(s)."
+oarsub -I -t deploy -l nodes=$nbr_nodes,walltime=$tmps -n "$nom"
