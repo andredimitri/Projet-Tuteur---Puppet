2aa727bdad5c373305869d5a2405db47bae8e0a5
Riwan Blonde <blond0392@etudiant.univ-nancy2.fr>
2012-03-02 12:07:51 +0100

    Mise a jour du scrpt d'installation automatique (marche parfaitement) modification apporté sur le module bind suite a l'entretien avec Sebastien Badia

---
+++ script/tunnel.sh
@@ -0,0 +1,9 @@
+#!/bin/bah
+
+user=`cat username`
+gateway=`/sbin/ip route | awk '/default/{ print $3 }'`
+
+ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PreferredAuthentications=publickey -o BatchMode=yes -NL 8080:proxy:3128 $user@$gateway &>/dev/null &
+
+rm /etc/apt/apt.conf.d/proxy*
+echo 'Acquire::http::Proxy "http://127.0.0.1:8080";' > /etc/apt/apt.conf.d/proxy
