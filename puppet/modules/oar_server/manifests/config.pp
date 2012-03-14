12c7792598adc7b798c1cd76ecddc6b7d95ee445
Qdexi94 <qdexheimer@gmail.com>
2012-03-14 08:46:48 +0100

    Modification recette OAR

--- puppet/modules/oar_server/manifests/config.pp
+++ puppet/modules/oar_server/manifests/config.pp
@@ -3,8 +3,10 @@ class oar_server::config {
 		ensure => present,
 		require => Class["oar_server::install"],
 		notify => Class["oar_server::service"],
+		source => "puppet:///oar_server/{monika,drawgantt}.cgi",
 	}
 	file { "(oar) ~/.ssh/{config,authorized_keys,id_rsa}":
 		ensure => present,
-		pattern => "Almigthy"
+		pattern => "Almigthy",
+	}
 }
